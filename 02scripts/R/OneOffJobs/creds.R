2b8024b75a1e0c63c029a762eeae25c28509f9e4
frank0434 <jian.liu@plantandfood.co.nz>
2020-07-01 22:14:05 +1200

    National leaflet yield map

---
+++ 02scripts/R/OneOffJobs/creds.R
@@ -0,0 +1,13 @@
+
+P009_LucerneNZ <- list(dbname = "cflfcl_ATLAS_P009_LucerneNZ",
+                       host = "database.powerplant.pfr.co.nz",
+                       username = "cflfcl_cflfcl_ATLAS_P009_LucerneNZ",
+                       password = "LsqBGnTizdRnBvJ0")
+# P007_MaizeNZ_DEMO_DB <- list(dbname = "postgres",
+#                              host = "dbP007Demo",
+#                              username = "postgres",
+#                              password = "password")
+# P009_LucerneNZ_DEMO_DB <- list(dbname = "postgres",
+#                                host = "dbP009Demo",
+#                                username = "postgres",
+#                                password = "password")
